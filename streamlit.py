83d325b6c6556b7daf6c1da6e4baeb9e736bfba7
Vijay Vankayalapati <83641668+vijayv500@users.noreply.github.com>
2021-09-18 04:33:38 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,41 @@
+import pandas as pd 
+import streamlit as st 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from collections import Counter
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.title('Builders In Your Locality: Hyderabad')
+
+st.markdown("""
+This app displays list of builders in your locality as word cloud.
+* **Data source:** Magicbricks
+""")
+
+st.sidebar.header('Select Your Locality')
+
+
+df = pd.read_csv('merge_first_second_raw_10907x23.csv')
+df['locality'] = df['locality'].astype(str)
+loc_list = list(df['locality'].unique())[0:10]
+
+
+chosen_loc = st.sidebar.selectbox('Locality',loc_list)
+
+df_chosen_loc = df 
+df_chosen_loc['builder'] = df_chosen_loc['builder'].astype(str)
+builder_list  = df_chosen_loc['builder'].values.tolist()
+
+count = Counter(builder_list)
+wordcloud = WordCloud(width = 1600, height = 800)\
+.generate_from_frequencies(count)
+plt.figure(figsize=(40,30))
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.show() 
+
+
+
+st.header('Display Companies in Selected locality')
+st.pyplot()
